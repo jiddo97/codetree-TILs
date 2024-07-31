78b1160c9b1770a77487d4ff6b89ecbf0989f0f8
jiddo97 <157673533+jiddo97@users.noreply.github.com>
2024-07-31 14:41:08 +0900

    Title: 배열의 특정 위치 찾기 2, Time: 163ms, Memory: 24MB, Status: Passed - CodeTree

--- 240731/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.py
+++ 240731/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.py
@@ -5,4 +5,7 @@ for i in range(0,10,2):
     sumval1+=arr[i]
 for j in range(1,10,2):
     sumval2+=arr[j]
-print(sumval2-sumval1)
+if sumval1>=sumval2:
+    print(sumval1-sumval2)
+else:
+    print(sumval2-sumval1)
