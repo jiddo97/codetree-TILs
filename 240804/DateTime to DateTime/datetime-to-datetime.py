a8c6e7b9c017600615b803e09e261cb4b02a8026
jiddo97 <157673533+jiddo97@users.noreply.github.com>
2024-08-04 15:27:11 +0900

    Title: DateTime to DateTime, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240804/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,20 @@
+a,b,c=map(int,input().split())
+elapsed=0
+d,e,f =11,11,11
+while True:
+    if d==a and e==b and f==a:
+        break
+    elif a < d and b <e and c<f:
+        print(-1)
+        break
+
+    elapsed+=1
+    f+=1
+    if f==60:
+        e+=1
+        f=0
+        if e==24:
+            d+=1
+            e=0
+    
+print(elapsed+2)
