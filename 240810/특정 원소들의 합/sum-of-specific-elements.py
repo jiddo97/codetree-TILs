280ad50b5b9a1a5afa25d21540b15581bd4c64a1
jiddo97 <157673533+jiddo97@users.noreply.github.com>
2024-08-10 15:48:16 +0900

    Title: 특정 원소들의 합, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240810/특정 원소들의 합/sum-of-specific-elements.py
+++ 240810/특정 원소들의 합/sum-of-specific-elements.py
@@ -1,5 +1,5 @@
 grid = [list(map(int, input().split())) for _ in range(4)]
 
-sum_colored = grid[0][0] + grid[1][1] + grid[2][2] + grid[3][3]
+sum_colored = grid[0][0] + grid[1][1] + grid[2][2] + grid[3][3] +grid[1][0]+grid[2][0]+grid[3][0]+grid[2][1]+grid[3][1]+grid[3][2]
 
 print(sum_colored)
